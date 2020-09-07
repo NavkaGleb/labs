f640cf692cf805556929fb862e1e8fbffeb4e40a
Gleb <navka.gleb@gmail.com>
2020-09-07 18:04:20 +0300

    add ListGraph class

---
+++ 3term/lab1/include/DataStructures/ListGraph.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+namespace ng {
+
+    class ListGraph {
+    public:
+        // constructor / destructor
+
+        // public methods
+
+    private:
+        // variables
+
+        // private methods
+
+    }; // class ListGraph
+
+} // namespace ng
