e1e73e1c769be379b6d14fe9dae915b1a0a3bef3
Gleb <navka.gleb@gmail.com>
2020-09-13 17:47:03 +0300

    rewrite .gitignore

---
+++ 3term/algorithms/lab1/include/Random.hpp
@@ -0,0 +1,52 @@
+#pragma once
+
+#include <random>
+#include <typeinfo>
+#include <ctime>
+#include <cmath>
+
+namespace ng {
+
+    class random {
+    public:
+        // static methods
+        template <typename T>
+        static T irand(const T& left, const T& right);
+
+        template <typename T>
+        static T frand(const T& left, const T& right, const int& fpart = 1);
+
+    }; // class rand
+
+    template <typename T>
+    T random::irand(const T& left, const T& right) {
+
+        static std::random_device rd;
+        static std::seed_seq seq{rd(), static_cast<unsigned>(time(nullptr))};
+        static std::mt19937 mt(seq);
+
+        std::uniform_int_distribution dist(left, right);
+        return dist(mt);
+
+    }
+
+    template <typename T>
+    T random::frand(const T& left, const T& right, const int& fpart) {
+
+        static std::random_device rd;
+        static std::seed_seq seq{rd(), static_cast<unsigned>(time(nullptr))};
+        static std::mt19937 mt(seq);
+
+        std::uniform_real_distribution dist(left, right);
+
+        if (typeid(T) == typeid(float))
+            return roundf(dist(mt) * std::pow(10, fpart)) / std::pow(10, fpart);
+
+        if (typeid(T) == typeid(double))
+            return round(dist(mt) * std::pow(10, fpart)) / std::pow(10, fpart);
+
+        return roundl(dist(mt) * std::pow(10, fpart)) / std::pow(10, fpart);
+
+    }
+
+} // namespace ng
