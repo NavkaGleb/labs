d416d695aef56894a89b6ca0e5bffbee544f797a
Navka Gleb <navka.gleb@gmail.com>
2020-10-28 23:38:38 +0200

    add Benchmark color

--- 3term/algorithms/lab4/src/main.cpp
+++ 3term/algorithms/lab4/src/main.cpp
@@ -8,28 +8,28 @@ int main() {
     Ng::Solution<int> solution;
     std::vector<decltype(solution)::Pair> data;
 
-    solution.Rand(20000);
+    solution.Rand(10);
     data = solution.Data();
 
     std::cout << Color::Green << "Init" << Color::Reset << std::endl;
     std::cout << solution << std::endl;
     std::cout << std::endl;
 
-    std::cout << Color::Blue << "Sort12 (O(n), stable)" << Color::Reset << std::endl;
-    std::cout << Ng::Benchmark([&solution] { solution.Sort12(); }) << " ms" << std::endl;
-//    std::cout << solution << std::endl;
+    std::cout << Color::Blue << "Sort12 (O(n), stable)" << std::endl;
+    std::cout << Color::Magenta << Ng::Benchmark([&solution] { solution.Sort12(); }) << " ms" << Color::Reset << std::endl;
+    std::cout << solution << std::endl;
     std::cout << std::endl;
 
-    std::cout << Color::Blue << "Sort13 (O(n), extra memory O(1))" << Color::Reset << std::endl;
+    std::cout << Color::Blue << "Sort13 (O(n), extra memory O(1))" << std::endl;
     solution.Data(data);
-    std::cout << Ng::Benchmark([&solution] { solution.Sort13(); }) << " ms" << std::endl;
-//    std::cout << solution << std::endl;
+    std::cout << Color::Magenta << Ng::Benchmark([&solution] { solution.Sort13(); }) << " ms" << Color::Reset << std::endl;
+    std::cout << solution << std::endl;
     std::cout << std::endl;
 
     std::cout << Color::Blue << "Sort23 (stable, extra memory O(1))" << Color::Reset << std::endl;
     solution.Data(data);
-    std::cout << Ng::Benchmark([&solution] { solution.Sort23(); }) << " ms" << std::endl;
-//    std::cout << solution << std::endl;
+    std::cout << Color::Magenta << Ng::Benchmark([&solution] { solution.Sort23(); }) << " ms" << Color::Reset << std::endl;
+    std::cout << solution << std::endl;
     std::cout << std::endl;
 
     return 0;
