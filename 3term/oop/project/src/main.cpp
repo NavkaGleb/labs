73f3a4ac36539bcb47eded918601e224f156eb7b
Navka Gleb <navka.gleb@gmail.com>
2020-11-05 18:24:38 +0200

    add basic Font

--- 3term/oop/project/src/main.cpp
+++ 3term/oop/project/src/main.cpp
@@ -1,23 +1,8 @@
-#include <iostream>
-
-#include <SFML/Graphics.hpp>
-#include <SFML/Window.hpp>
-#include <SFML/System.hpp>
+#include "Application/Application.hpp"
 
 int main() {
-    sf::RenderWindow window(sf::VideoMode(500, 500), "SFML");
-    window.setFramerateLimit(60);
-
-    while (window.isOpen()) {
-        sf::Event event{};
-
-        while (window.pollEvent(event))
-            if (event.type == sf::Event::Closed)
-                window.close();
-
-        window.clear();
-        window.display();
-    }
+    ng::Application application;
+    application.run();
 
     return 0;
 }
