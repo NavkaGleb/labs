2bb2078e09e10075ff675b3d31cd58cd740dd3a5
Navka Gleb <navka.gleb@gmail.com>
2020-11-05 08:56:50 +0200

    init oop/project. link with SFML lib

--- 3term/oop/project/src/main.cpp
+++ 3term/oop/project/src/main.cpp
@@ -1,6 +1,23 @@
 #include <iostream>
 
+#include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/System.hpp>
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    sf::RenderWindow window(sf::VideoMode(500, 500), "SFML");
+    window.setFramerateLimit(60);
+
+    while (window.isOpen()) {
+        sf::Event event{};
+
+        while (window.pollEvent(event))
+            if (event.type == sf::Event::Closed)
+                window.close();
+
+        window.clear();
+        window.display();
+    }
+
     return 0;
 }
