1779cd56133e79a8728634a5afa1ab0020dfaa6a
Navka Gleb <navka.gleb@gmail.com>
2020-11-05 18:25:04 +0200

    add ng::Application class

---
+++ 3term/oop/project/src/Application/Application.cpp
@@ -0,0 +1,81 @@
+#include "Application.hpp"
+
+#include <iostream>
+#include <cmath>
+
+#include "States/MandelbrotState.hpp"
+
+namespace ng {
+
+    // constructor / destructor
+    Application::Application() :
+        m_window(sf::VideoMode(1280, 720), "SFML", sf::Style::Close) {
+
+        // window
+        m_window.setFramerateLimit(60);
+        m_window.setVerticalSyncEnabled(true);
+
+        // font
+        if (!m_font.loadFromFile("../media/Fonts/Baloo2-Medium.ttf"))
+            throw std::invalid_argument("Application::Application: failed to load font");
+
+        // text
+        m_statisticsText.setFont(m_font);
+        m_statisticsText.setPosition(std::floor(5.f), std::floor(5.f));
+        m_statisticsText.setCharacterSize(20);
+        m_statisticsText.setFillColor(sf::Color::White);
+
+        // states
+        m_states.emplace(std::make_unique<MandelbrotState>());
+    }
+
+    // public methods
+    void Application::run() {
+        while (m_window.isOpen()) {
+            updateFrameTime();
+            pollEvent();
+            update();
+            render();
+        }
+    }
+
+    // member methods
+    void Application::updateFrameTime() {
+        m_lastFrameTime = m_frameTime;
+        m_frameTime = m_clock.restart();
+    }
+
+    void Application::pollEvent() {
+        sf::Event event{};
+
+        while (m_window.pollEvent(event)) {
+            switch (event.type) {
+                case sf::Event::Closed:
+                    m_window.close();
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+
+    void Application::update() {
+        m_statisticsText.setString(
+            "FPS: " + std::to_string((1.f / m_frameTime.asSeconds() - m_lastFrameTime.asSeconds()))
+        );
+
+        if (!m_states.empty())
+            m_states.top()->update(m_frameTime.asSeconds());
+    }
+
+    void Application::render() {
+        m_window.clear();
+        m_window.draw(m_statisticsText);
+
+        if (!m_states.empty())
+            m_states.top()->render(m_window);
+
+        m_window.display();
+    }
+
+} // namespace ng
---
+++ 3term/oop/project/src/Application/Application.hpp
@@ -0,0 +1,40 @@
+#pragma once
+
+#include <stack>
+#include <memory>
+
+#include <SFML/Window.hpp>
+#include <SFML/Graphics.hpp>
+
+#include "States/State.hpp"
+
+namespace ng {
+
+    class Application {
+    public:
+        // constructor / destructor
+        Application();
+        virtual ~Application() = default;
+
+        // public methods
+        void run();
+
+    private:
+        // member data
+        sf::RenderWindow m_window;
+        sf::Clock m_clock;
+        sf::Time m_lastFrameTime;
+        sf::Time m_frameTime;
+        sf::Font m_font;
+        sf::Text m_statisticsText;
+        std::stack<std::unique_ptr<State>> m_states;
+
+        // member methods
+        void updateFrameTime();
+        void pollEvent();
+        void update();
+        void render();
+
+    }; // class Application
+
+} // namespace ng
