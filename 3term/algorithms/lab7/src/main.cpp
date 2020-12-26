bc36b5b87f4429f4f16b317beabd0076a77d04d0
NavkaGleb <navka.gleb@gmail.com>
2020-12-26 14:22:38 +0200

    Labs/algorithms/lab7: Remove Random.hpp

--- 3term/algorithms/lab7/src/main.cpp
+++ 3term/algorithms/lab7/src/main.cpp
@@ -1,30 +1,8 @@
 #include <iostream>
 
 #include "RabinKarp/MatrixRabinKarp.hpp"
-#include "Random/Random.hpp"
 
 int main() {
-//    Ng::Matrix<char> pattern = {
-//        { '3', 'd' },
-//    };
-//
-//    Ng::Matrix<char> text = {
-//        { 'a', 'b', 'c', 't' },
-//        { '1', '2', '3', 'd' },
-//        { 'd', 'f', 'g', 'k' },
-//        { 'a', 'b', 'c', 'i' },
-//    };
-//
-//    Ng::MatrixRabinKarp rabinKarp(pattern);
-//
-//    auto positions = rabinKarp.Search(text);
-//
-//    if (positions.empty())
-//        std::cout << "Nothing found" << std::endl;
-//
-//    for (const auto& position : positions)
-//        std::cout << "i = " << position.first << ", j = " << position.second << std::endl;
-
     std::size_t sizeText = 40;
     std::size_t sizePattern = 3;
 
