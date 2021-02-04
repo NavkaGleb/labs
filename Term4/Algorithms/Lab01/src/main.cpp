55608256efe4df8880ba7dcfddabe595c9d0d0c5
NavkaGleb <navka.gleb@gmail.com>
2021-02-05 00:18:01 +0200

    Try to implement 6th variant

--- Term4/Algorithms/Lab01/src/main.cpp
+++ Term4/Algorithms/Lab01/src/main.cpp
@@ -3,35 +3,29 @@
 
 #include "StaticHashTable.hpp"
 
+enum Category : int { Sport = 0, Policy, Science };
+
+std::ostream& operator <<(std::ostream& ostream, const Category& category) {
+    switch (category) {
+        case Category::Sport:   ostream << "Sport";   break;
+        case Category::Policy:  ostream << "Policy";  break;
+        case Category::Science: ostream << "Science"; break;
+    }
+
+    return ostream;
+}
+
 int main() {
-    Ng::StaticHashTable<int, std::string, 10> hashTable;
+    Ng::StaticHashTable<std::string, Category, 10> hashTable;
 
-//    for (int i = 0; i < 6; ++i) {
-//        int rand = Ng::Random::Get(0, 100);
-//        std::cout << rand << std::endl;
-//
-//        auto& [key, value] = *hashTable.Push(rand, std::to_string(Ng::Random::Get(0, 10)));
-//    }
-//
-//    hashTable.Print();
-
-    for (int i = 0; i < 10; ++i)
-        hashTable[i] = std::to_string(i);
+    for (int i = 0; i < 10; ++i) {
+        auto& [key, value] = *hashTable.Push(
+            std::to_string(i),
+            static_cast<Category>(Ng::Random::Get(0, 2))
+        );
+    }
 
     hashTable.Print();
-    for (const auto& [key, value] : hashTable)
-        std::cout << "Key: " << key << ", Value: " << value << std::endl;
-
-    std::cout << std::endl;
-
-    for (int i = 0; i < 2; ++i) {
-        int n;
-
-        std::cin >> n;
-
-        hashTable.Pop(n);
-        hashTable.Print();
-    }
 
     for (const auto& [key, value] : hashTable)
         std::cout << "Key: " << key << ", Value: " << value << std::endl;
