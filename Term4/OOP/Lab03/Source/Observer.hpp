a668171ac8552cd06aeda077fa98c981084ba470
NavkaGleb <navka.gleb@gmail.com>
2021-05-26 00:27:52 +0300

    OOP/Lab03: Add base of Observer Pattern. Try to embed Observer to SortLayer class

---
+++ Term4/OOP/Lab03/Source/Observer.hpp
@@ -0,0 +1,41 @@
+#pragma once
+
+namespace Lab03 {
+
+    class IObserver {
+    public:
+        IObserver() = default;
+        virtual ~IObserver() = default;
+
+    public:
+        virtual void OnNotify(const void* data) = 0;
+
+    }; // class IObserver
+
+    template <typename T>
+    concept ObserverConcept = std::is_base_of_v<IObserver, T>;
+
+    class Subject {
+    public:
+        Subject() = default;
+        virtual ~Subject() = default;
+
+    public:
+        template <ObserverConcept T>
+        IObserver* RegisterObserver();
+
+        void RegisterObserver(IObserver* observer);
+        void UnregisterObserver(IObserver* observer);
+        void NotifyObservers(const void* eventData);
+
+    private:
+        std::vector<IObserver*> m_Observers;
+
+    }; // class Subject
+
+    template <ObserverConcept T>
+    IObserver* Subject::RegisterObserver() {
+        return m_Observers.emplace_back(new T);
+    }
+
+} // namespace Lab03
