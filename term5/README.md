3ec66729b6cab15a3ec0e4874eb58d09629ba7b8
Gleb <navka.gleb@gmail.com>
2021-10-24 22:10:28 +0300

    term5/os/lab2: Create project skeleton

--- term5/README.md
+++ term5/README.md
@@ -3,4 +3,5 @@
 * dc - distributed calculations
 * nm - numerical methods
 * os - operating systems
+* rb - ruby
 * sm - system modeling
---
+++ term5/os/lab2/src/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+namespace os_lab2 {
+
+void HelloOsLab2() {
+  std::cout << "Hello, os lab2" << std::endl;
+}
+
+} // namespace os_lab2
+
+int main() {
+  os_lab2::HelloOsLab2();
+}
