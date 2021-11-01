43f19b30c266602ba09fb46eab737b0902ac2522
Gleb <navka.gleb@gmail.com>
2021-11-01 13:36:16 +0200

    term5/sm/lab1: Fix bug with frequencies. Update report

--- term5/sm/lab1/main.py
+++ term5/sm/lab1/main.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+from sklearn.metrics import mean_squared_error
 from scipy.signal import argrelextrema
 
 
@@ -21,17 +22,23 @@ def main():
     transformed_data = (m @ measurements) / n
     transformed_half_data = transformed_data[:transformed_data.shape[0] // 2 - 1]
 
-    # Main frequency
-    main_frequency = np.array(argrelextrema(transformed_half_data, np.greater))[0][0] / t
+    # Frequency
+    extremes = np.array(
+        argrelextrema(transformed_half_data, np.greater)
+    )
+    frequency1 = extremes[0][0] / t
+    frequency2 = extremes[0][1] / t
 
-    print(f'main_frequency: {main_frequency}')
+    print(f'frequency1: {frequency1}')
+    print(f'frequency2: {frequency2}')
 
     # Least square method
     c = np.array([
         np.sum(measurements * time ** 3),
         np.sum(measurements * time ** 2),
         np.sum(measurements * time),
-        np.sum(measurements * np.sin(2. * np.pi * main_frequency * time)),
+        np.sum(measurements * np.sin(2. * np.pi * frequency1 * time)),
+        np.sum(measurements * np.sin(2. * np.pi * frequency2 * time)),
         np.sum(measurements)
     ])
 
@@ -40,7 +47,9 @@ def main():
     functions = [
         time ** 3,
         time ** 2,
-        time, np.sin(2.0 * np.pi * main_frequency * time),
+        time,
+        np.sin(2.0 * np.pi * frequency1 * time),
+        np.sin(2.0 * np.pi * frequency2 * time),
         np.ones(n)
     ]
 
@@ -53,6 +62,10 @@ def main():
 
     print(f'solution: {solution}')
     approximated_functions = solution @ functions
+
+    mse = mean_squared_error(measurements, approximated_functions)
+
+    print(f'mse: {mse}')
 
     # Draw a chart
     figure, axis = plt.subplots(3)
