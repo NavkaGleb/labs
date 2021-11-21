3c6019e89426e85954ed159d070617c16f014b50
Gleb <navka.gleb@gmail.com>
2021-11-21 23:32:53 +0200

    term5/nm/lab4: Fix bug in chebyshev_polynomial_zero function

--- term5/nm/lab4/main.py
+++ term5/nm/lab4/main.py
@@ -9,21 +9,28 @@ import interpolation_newton_polynomial
 
 
 def f(x):
-    return 3 ** x
+    return math.sin(x ** 2) * math.tan(x)
 
 
-def chebyshev_polynomial_zero(k, n):
-    return math.cos(((2 * k + 1) * math.pi) / (2 * n))
+def chebyshev_polynomial_zero(k, n, a, b):
+    return (a + b) / 2 + (b - a) / 2 * math.cos(((2 * k + 1) * math.pi) / (2 * n))
+
+
+def print_measurements(measurements):
+    for i in range(len(measurements)):
+        print(f'{i}:\t{measurements[i]:.20f}')
 
 
 def main():
     node_count = 9
+    a = -10
+    b = 10
 
     nodes = []
 
     # Init nodes
     for i in range(node_count):
-        nodes.append(chebyshev_polynomial_zero(i, node_count))
+        nodes.append(chebyshev_polynomial_zero(i, node_count, a, b))
 
     # Find interpolation polynomials
     lagrange_form = interpolation_lagrange_polynomail.find_lagrange_form(nodes, f)
@@ -39,6 +46,14 @@ def main():
         lagrange_form_measurements[i] = lagrange_form(nodes[i])
         newton_form_measurements[i] = newton_form(nodes[i])
 
+        print(
+            f'{i}:\t{f_measurements[i]:.30f}\t'
+            f'{lagrange_form_measurements[i]:.30f}\t'
+            f'{newton_form_measurements[i]:.30f}'
+        )
+
+    print('--------------------')
+
     # Find mean squared error
     lagrangian_form_mse = mean_squared_error(f_measurements, lagrange_form_measurements)
     newton_form_mse = mean_squared_error(f_measurements, newton_form_measurements)
@@ -53,7 +68,7 @@ def main():
     figure.set_size_inches(10.5, 10.5, forward=True)
     figure.set_dpi(100)
 
-    axis[0].set_title('f(x) = 3 ^ x')
+    axis[0].set_title('f(x) = math.sin(x ** 2) * math.tan(x)')
     axis[0].plot(nodes, f_measurements, 'o-', linewidth=2)
 
     axis[1].set_title('Lagrange form')
